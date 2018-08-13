639a988a651492fdf505d56d18919e7c73b69ac2
Andy M <andy@andypants.xyz>
2018-08-12 21:39:56 -0700

    Added new celeb input, and styling

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,11 +1,19 @@
 $(document).ready(function(){
 
     var buttons = $("<div>");
-    var topics = ["Lady Gaga", "Katy Perry"]
+    var topics = ["Lady Gaga", "Katy Perry", "Emma Stone", "Bill Murray", "Jennifer Lawrence", "Paul Rudd", "Rebel Wilson", 
+                "Jason Bateman", "Aubrey Plaza", "Bradley Cooper", "Robert Downey Jr", "Jim Carrey", "Tina Fey", "Weird Al", 
+                "Kristen Wiig", "Will Ferrell", "Melissa McCarthy", "Donald Glover", "Elijah Wood"];
     var gifsView = $("<div>");
     gifsView.addClass("gifs");
     var input = $("<div>");
     input.addClass("input");
+    var form = $("<form>").attr("id", "celebrity-form");
+    form.append("<label for='celeb-input'>Add a Celebrity!</label><br>"+
+                "<input type='text' id='celeb-input' width='100%'><br>"+
+                "<input id='add-celeb' type='submit' class='btn-primary' value='Add Celebrity'>");
+    input.append(form);
+
 
     function renderButtons(){
         buttons.empty();
@@ -13,7 +21,7 @@ $(document).ready(function(){
             //create a button
             var btn = $("<button>");
             // Adds a class of artist
-            btn.addClass("btn artist");
+            btn.addClass("btn");
             // Added a data-attribute
             btn.attr("data-name", topics[i]);
             // Provided the initial button text
@@ -33,8 +41,8 @@ $(document).ready(function(){
             method: "GET"
           })
             .then(function(response) {
-                console.log(response);
                 var results = response.data;
+                $(".gifs").empty();
 
                 for (var i = 0; i < results.length; i++) {
                     var gifDiv = $("<div class='item'>");
@@ -70,6 +78,18 @@ $(document).ready(function(){
           $(this).attr("src", $(this).attr("data-still"));
           $(this).attr("data-state", "still");
         }
+    })
+
+    $("#content").on("click", "#add-celeb", function(event){
+        event.preventDefault();
+
+        var celeb = $("#celeb-input").val().trim();
+        $("#celeb-input").val("");
+        if(topics.indexOf(celeb) === -1 && celeb.length > 0){
+            topics.push(celeb);
+            renderButtons();
+        }
+
     })
 
     $("#content").append(buttons);
